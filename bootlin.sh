e8bad6da9b17a45af15bfd7c3dff1c3e6d94bad4
Andrzej Jarzebowski <jarzebowski.andrzej@gmail.com>
2015-07-26 22:47:55 +0100

    Initial commit

---
+++ bootlin.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+convertImage () {
+	set -o verbose
+	hdiutil convert -format UDRW -o $1 $1
+	set +o verbose
+}
+
+createBootableUSB () {
+	echo "To create bootable USB drive I need ROOT privileges. Please provide your password."
+	echo "After providing the password dd command will run. Unfortunatelly dd DOES NOT have any progress bar so despite the fact that nothing is showed, the script actually works, please be patient."
+	set -o verbose
+	sudo dd if=$1.dmg of=/dev/r$2 bs=1m
+	set +o verbose
+}
+
+regex='^.*\.iso$'
+
+if [[ $1 =~ $regex ]]; 
+	then
+		echo "$1 will be use to create bootable USB"
+		echo "Please select target USB drive by typing in IDENTIFIER of your USB drive (eg. disk1):"
+		set -o verbose
+		diskutil list
+		set +o verbose
+		read driveName
+		echo "Are you sure that you want to use $driveName as bootable USB? All data on the device will be erased! [Y/n]"
+			read driveQuestion
+			if [[ $driveQuestion == "Y" ]] || [[ $driveQuestion == "y" ]] || [[ $driveQuestion == "yes" ]]; 
+				then
+				echo "Unmounting $driveName"
+				umount $driveName | cut -c1-5
+				echo "Converting $1 to DMG format:"
+				convertImage $1
+				echo "Creating bootable USB on $driveName"
+				createBootableUSB $1 $driveName | cut -c1-5
+			elif [[ $driveQuestion == "N" ]] || [[ $driveQuestion == "n" ]] || [[ $driveQuestion == "no" ]]; 
+				then
+				echo no
+			else
+				exit
+			fi
+	else
+		echo "Provided file is not in ISO format!"
+fi
